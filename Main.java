cb3e065a199a067c0dafac19d960db50d8091ea0
Magyar Dávid <57763255+Keelorath@users.noreply.github.com>
2021-02-12 19:12:02 +0100

    Update combat in main

--- Main.java
+++ Main.java
@@ -6,42 +6,42 @@ public class Main {
         Entity combatant_2 = new Warrior(90, 35, 15);
         Entity combatant_mage = new Mage(50, 70, 5, 100);
 
-        battle(combatant_1, combatant_2);
+        battle(combatant_1, combatant_mage);
     }
 
-    private static void battle(Entity good, Entity evil) {
+    private static void battle(Entity warrior, Entity mage) {
         Random rand = new Random();
         int i = 1;
+        System.out.println("Warrior - HP: " + warrior.getHealth() + ", ATK: " + warrior.getAttack() + ", DEF: "
+                + warrior.getDefense());
         System.out.println(
-                "Good - HP: " + good.getHealth() + ", ATK: " + good.getAttack() + ", DEF: " + good.getDefense());
-        System.out.println(
-                "Evil - HP: " + evil.getHealth() + ", ATK: " + evil.getAttack() + ", DEF: " + evil.getDefense());
+                "Mage - HP: " + mage.getHealth() + ", ATK: " + mage.getAttack() + ", DEF: " + mage.getDefense());
         System.out.println("====================");
         System.out.println("The Battle begins.");
-        while (good.isAlive() && evil.isAlive()) {
+        while (warrior.isAlive() && mage.isAlive()) {
             System.out.println("********************");
             System.out.println(i + ". round:");
             if (rand.nextBoolean()) {
-                System.out.println("Good attacks first.");
-                good.attack(evil);
-                if (evil.isAlive()) {
-                    evil.attack(good);
+                System.out.println("The Warrior attacks first.");
+                warrior.attack(mage);
+                if (mage.isAlive()) {
+                    mage.attack(warrior);
                 }
             } else {
-                System.out.println("Evil attacks first.");
-                evil.attack(good);
-                if (good.isAlive()) {
-                    good.attack(evil);
+                System.out.println("The Mage attacks first.");
+                mage.attack(warrior);
+                if (warrior.isAlive()) {
+                    warrior.attack(mage);
                 }
             }
             i++;
             System.out.println("********************");
-            System.out.println("Good - HP: " + good.getHealth());
-            System.out.println("Evil - HP: " + evil.getHealth());
+            System.out.println("Warrior - HP: " + warrior.getHealth());
+            System.out.println("Mage - HP: " + mage.getHealth());
         }
         System.out.println("====================");
         System.out.println("The Battle is over.");
-        String result = good.isAlive() ? "The Good won!" : "The Evil won!";
+        String result = warrior.isAlive() ? "The Warrior won!" : "The Mage won!";
         System.out.println(result);
     }
 }
