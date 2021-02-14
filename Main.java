f726476c9612013862997bf79953fb607b1a792e
vadalf <78406640+vadalf@users.noreply.github.com>
2021-02-14 16:49:59 +0100

    Revert commits to simplify class hierarchy
    This commit reverts the following commits:
    
    1e2c9fa Factor common code out into a parent class
    cb3e065 Update combat in main
    95091cb Add Mage entity
    50b43fe Add the base interface for entities

--- Main.java
+++ Main.java
@@ -4,44 +4,43 @@ public class Main {
     public static void main(String[] args) {
         Entity combatant_1 = new Warrior(75, 45, 10);
         Entity combatant_2 = new Warrior(90, 35, 15);
-        Entity combatant_mage = new Mage(50, 70, 5, 100);
 
-        battle(combatant_1, combatant_mage);
+        battle(combatant_1, combatant_2);
     }
 
-    private static void battle(Entity warrior, Entity mage) {
+    private static void battle(Entity good, Entity evil) {
         Random rand = new Random();
         int i = 1;
-        System.out.println("Warrior - HP: " + warrior.getHealth() + ", ATK: " + warrior.getAttack() + ", DEF: "
-                + warrior.getDefense());
         System.out.println(
-                "Mage - HP: " + mage.getHealth() + ", ATK: " + mage.getAttack() + ", DEF: " + mage.getDefense());
+                "Good - HP: " + good.getHealth() + ", ATK: " + good.getAttack() + ", DEF: " + good.getDefense());
+        System.out.println(
+                "Evil - HP: " + evil.getHealth() + ", ATK: " + evil.getAttack() + ", DEF: " + evil.getDefense());
         System.out.println("====================");
         System.out.println("The Battle begins.");
-        while (warrior.isAlive() && mage.isAlive()) {
+        while (good.isAlive() && evil.isAlive()) {
             System.out.println("********************");
             System.out.println(i + ". round:");
             if (rand.nextBoolean()) {
-                System.out.println("The Warrior attacks first.");
-                warrior.attack(mage);
-                if (mage.isAlive()) {
-                    mage.attack(warrior);
+                System.out.println("Good attacks first.");
+                good.attack(evil);
+                if (evil.isAlive()) {
+                    evil.attack(good);
                 }
             } else {
-                System.out.println("The Mage attacks first.");
-                mage.attack(warrior);
-                if (warrior.isAlive()) {
-                    warrior.attack(mage);
+                System.out.println("Evil attacks first.");
+                evil.attack(good);
+                if (good.isAlive()) {
+                    good.attack(evil);
                 }
             }
             i++;
             System.out.println("********************");
-            System.out.println("Warrior - HP: " + warrior.getHealth());
-            System.out.println("Mage - HP: " + mage.getHealth());
+            System.out.println("Good - HP: " + good.getHealth());
+            System.out.println("Evil - HP: " + evil.getHealth());
         }
         System.out.println("====================");
         System.out.println("The Battle is over.");
-        String result = warrior.isAlive() ? "The Warrior won!" : "The Mage won!";
+        String result = good.isAlive() ? "The Good won!" : "The Evil won!";
         System.out.println(result);
     }
 }
