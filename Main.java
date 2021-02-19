f2e860c2ed66c5001924852bbebeb00bb09e5138
tmRobi <tothmarkrobert@gmail.com>
2021-02-19 11:29:01 +0100

    Restructure readDouble method

--- Main.java
+++ Main.java
@@ -2,29 +2,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double goodHealth = readDouble("Please give the health of the good entity: ");
-        double goodAttack = readDouble("Please give the attack of the good entity: ");
-        double goodDefense = readDouble("Please give the defense of the good entity: ");
+        Scanner in = new Scanner(System.in);
 
-        double evilHealth = readDouble("Please give the health of the good entity: ");
-        double evilAttack = readDouble("Please give the attack of the good entity: ");
-        double evilDefense = readDouble("Please give the defense of the good entity: ");
+        double goodHealth = readDouble(in, "Please give the health of the good entity: ");
+        double goodAttack = readDouble(in, "Please give the attack of the good entity: ");
+        double goodDefense = readDouble(in, "Please give the defense of the good entity: ");
+
+        double evilHealth = readDouble(in, "Please give the health of the good entity: ");
+        double evilAttack = readDouble(in, "Please give the attack of the good entity: ");
+        double evilDefense = readDouble(in, "Please give the defense of the good entity: ");
 
         Entity good = new Entity(goodHealth, goodAttack, goodDefense);
         Entity evil = new Entity(evilHealth, evilAttack, evilDefense);
 
         Battle bt = new Battle("Good", good, "Evil", evil);
         bt.battle();
+
+        in.close();
     }
 
-    static double readDouble(String msg) {
-        Scanner in = new Scanner(System.in);
+    static double readDouble(Scanner scanner, String msg) {
         boolean b = true;
         String valueString;
         double value = 0;
         do {
             System.out.print(msg);
-            valueString = in.nextLine();
+            valueString = scanner.nextLine();
             try {
                 value = Double.parseDouble(valueString);
                 b = false;
