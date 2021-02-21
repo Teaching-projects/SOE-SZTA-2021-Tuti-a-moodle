1438972a6fa3268dd268cb32ea72bff4dfb922ca
itsmehali <halaszbalazs9825@gmail.com>
2021-02-21 11:53:56 +0100

    Scanner message fixed on good and evil

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        double goodHealth = readDouble(in, "Please give the health of the good entity: ");
-        double goodAttack = readDouble(in, "Please give the attack of the good entity: ");
-        double goodDefense = readDouble(in, "Please give the defense of the good entity: ");
+        double goodHealth = readDouble(in, "Enter the health of the good entity: ");
+        double goodAttack = readDouble(in, "Enter the attack of the good entity: ");
+        double goodDefense = readDouble(in, "Enter the defense of the good entity: ");
 
-        double evilHealth = readDouble(in, "Please give the health of the good entity: ");
-        double evilAttack = readDouble(in, "Please give the attack of the good entity: ");
-        double evilDefense = readDouble(in, "Please give the defense of the good entity: ");
+        double evilHealth = readDouble(in, "Enter the health of the evil entity: ");
+        double evilAttack = readDouble(in, "Enter the attack of the evil entity: ");
+        double evilDefense = readDouble(in, "Enter the defense of the evil entity: ");
 
         Entity good = new Entity(goodHealth, goodAttack, goodDefense);
         Entity evil = new Entity(evilHealth, evilAttack, evilDefense);
