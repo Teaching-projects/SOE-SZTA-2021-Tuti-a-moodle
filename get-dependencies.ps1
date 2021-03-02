5600b1be33361fbac8a2f452fccd3991342e7f9d
vadalf <78406640+vadalf@users.noreply.github.com>
2021-03-02 20:19:30 +0100

    Add management scripts
    Add scripts to fetch dependencies, build using dependencies and run
    the program using dependencies.

---
+++ get-dependencies.ps1
@@ -0,0 +1,30 @@
+$OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+
+function Get-Downloader {
+    $Downloader = New-Object System.Net.WebClient
+    $Downloader.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+    $Downloader.Proxy = [System.Net.GlobalProxySelection]::GetEmptyWebProxy()
+
+    return $Downloader
+}
+
+function Get-RemoteFile([string] $Url, [string] $Filename) {
+    (Get-Downloader).DownloadFile($Url, $Filename)
+}
+
+$JacksonVer = '2.12.1'
+$JacksonJar = 'jackson-{{0}}-{0}.jar' -f $JacksonVer
+$JacksonUrl = 'https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-{{0}}/{0}/{1}' -f $JacksonVer, $JacksonJar
+
+if (Test-Path ($JacksonJar -f 'core') -PathType Leaf) {
+    echo "Jackson is already downloaded, exiting..."
+    exit 0
+}
+
+echo ("Downloading Jackson {0}..." -f $JacksonVer)
+Get-RemoteFile ($JacksonUrl -f 'core') ($JacksonJar -f 'core')
+Get-RemoteFile ($JacksonUrl -f 'databind') ($JacksonJar -f 'databind')
+Get-RemoteFile ($JacksonUrl -f 'annotations') ($JacksonJar -f 'annotations')
+
+echo ("Jackson {0} downloaded" -f $JacksonVer, $JacksonJar)
+exit 0
