c464268e1302cf4665e4ee7635eaa4c9570a2cc1
vadalf <78406640+vadalf@users.noreply.github.com>
2021-03-22 19:03:25 +0100

    Add Junit to the CI with a dummy test
    This test file is here only to serve as an example. It should be removed
    by whoever writes the unit tests.

---
+++ src/DummyTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// FIXME: remove this class
+public class DummyTest {
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
